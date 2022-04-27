c91ec726fa2efd9af1a8cd4b084e8d8a6a89f827
Veysel AKSIN <veyselaksn@gmail.com>
2022-04-28 00:35:16 +0300

    Main fun finish!

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 # TGA MAIN FUNC
 
-def checker(mystr:str):
-    open_brackets = "[{("
-    close_brackets = "]})"
-    
-    heap = list()
 
-    for i in mystr:
-        if i in open_brackets:
-            heap.append(i)
+class Checker:
 
-    print(heap)
+    open_brackets = ["{", "[", "("]
+    close_brackets = ["}", "]", ")"]
+    brackets = dict(zip(open_brackets, close_brackets))
+    stack = list()
 
-checker("{}")
+    def __init__(self, string):
+        self.string = string
+
+    def isBalanced(self):
+        
+        for i in self.string:
+            if i in self.open_brackets:
+                self.stack.append(i)
+
+            elif i in self.close_brackets:
+                if i != self.brackets[self.stack.pop()]:
+                    return "NO"
+
+        if not self.stack:
+            return "YES"
 
